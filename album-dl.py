0e63019389dd9a318bd9585b0b69c09534b74595
Ronan GAILLARD <ronan.gaillard@supelec.fr>
2016-02-21 16:24:14 +0100

    Added gitignore, nice and clear output in terminal/console with colors and progress, added retry to fetch video url

--- album-dl.py
+++ album-dl.py
@@ -15,11 +15,44 @@ import base64
 import unicodedata
 import string
 import argparse
+from subprocess import Popen, PIPE, STDOUT
 
 LAST_FM_API_KEY = '739148a56b222644ce68c68e3851b55a'
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 validFilenameChars = "-_.() %s%s" % (string.ascii_letters, string.digits)
 
+def printf(text):
+    sys.stdout.write(text)
+
+def update_status(song, track_number, status, type=None):
+    sys.stdout.write("\033[2K") # Clear to the end of line
+    line_to_print = ''
+    
+    if type == 'success':
+        line_to_print = bcolors.OKGREEN
+        
+    if type == 'error':
+        line_to_print = bcolors.FAIL
+        
+    if type == 'warning':
+        line_to_print = bcolors.WARNING
+        
+    line_to_print += '\r' + track_number + ' - ' + song_name + '  : ' + status
+    
+    if type != None:
+        line_to_print += bcolors.ENDC
+        
+    printf(line_to_print)
 
 def parse_arg():
     parser = argparse.ArgumentParser(description='Download full music album from the web')
@@ -37,8 +70,22 @@ def removeDisallowedFilenameChars(filename):
 
 
 def get_url_of_song(songname):
-    payload = {'search_query': songname}
-    r = requests.get('http://www.youtube.com/results', params=payload)
+    success = False
+    retry = 0
+    r = None
+    
+    while not success and retry <5:
+        payload = {'search_query': songname}
+        r = requests.get('http://www.youtube.com/results', params=payload)
+        
+        if r.status_code == requests.codes.ok:
+            success = True
+            
+        retry += 1
+        
+    if retry >= 5:
+        return None
+        
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})', r.text)
     return "http://www.youtube.com/watch?v=" + search_results[0]
     
@@ -70,29 +117,34 @@ def download_video(videourl, artist, album, song, track_number, nb_of_tracks):
     if not os.path.exists(album_directory):
         os.makedirs(album_directory)
         
-    
-        
-    print command_line
-    
     result = -1
     retry = 0
     
     while result != 0 and retry < 5: 
-        result = os.system(command_line)
+        #result = os.system(command_line)
+        youtubedl_command = Popen(command_line, stdout=PIPE, stderr=STDOUT)
+        stdout, nothing = youtubedl_command.communicate()    
+
+        with open('album-dl.log', 'w') as log:
+            log.write(stdout)
+        
+        result = youtubedl_command.returncode
        
         retry += 1
         
+        update_status(song ,track_number, 'Retrying to download', 'warning')
+        
     if retry == 5:
-        print 'FATAL ERROR : Unable to download : ' +song
+        update_status(song ,track_number, 'FATAL ERROR : Unable to download', 'error')
         return
         
     if os.path.exists(absolute_file_path):
         os.remove(absolute_file_path)
     
-    print 'moving from temp.m4a to ' + absolute_file_path
+    update_status(song ,track_number, 'Moving to right folder')
     os.rename("temp.m4a", absolute_file_path)
         
-    print '\t\tTagging : ' + absolute_file_path
+    update_status(song ,track_number, 'Tagging song')
     audio = MP4(absolute_file_path)
     audio['\xa9ART'] = artist #artist
     audio['\xa9alb'] = album
@@ -100,6 +152,8 @@ def download_video(videourl, artist, album, song, track_number, nb_of_tracks):
     audio['aART'] = artist #album artist
     audio['trkn'] = [(int(track_number), nb_of_tracks)]
     audio.save()
+    
+    update_status(song ,track_number, 'Done !', 'success')
 
 #to deal with xml encoded in UTF-8
 reload(sys)
@@ -107,32 +161,32 @@ sys.setdefaultencoding('utf-8')
 
 args = parse_arg()
 
-print 'Desired artist : ' + args.artist
-print 'Desired artist : ' + args.album
-
 print 'Contacting Last.fm to get album info...'
 payload = {'api_key': LAST_FM_API_KEY, 'artist': args.artist, 'album': args.album}
-
 response = requests.get('http://ws.audioscrobbler.com/2.0/?method=album.getInfo', payload)
-print 'Done, parsing...'
 tree = ET.ElementTree(ET.fromstring(response.text))
+
 print 'Album information :'
-#print response.
 album = tree.find('album/name').text
 artist = tree.find('album/artist').text
 nb_of_tracks = len(tree.find('album/tracks'))
+
 print 'Album :', album
 print 'Artist :', artist
 print 'Number of tracks :', nb_of_tracks
+
 print 'Album songs :'
 for song in tree.find('album/tracks'):
     song_name = song.find('name').text
     track_number = song.attrib['rank']
-    print '\t' + track_number + ' - ' + song_name
-    print '\t\tGetting url of song...'
+    update_status(song, track_number , 'Getting url of song...')
     songurl = get_url_of_song(artist + ' ' + song_name)
-    print '\r\t\tDownloading song...'
+    if songurl == None:
+        update_status(song, track_number , 'Unable to fetch url of song video after multiple retries', 'error')
+        break
+    update_status(song, track_number , 'Downloading and converting song')
     download_video(songurl, artist, album, song_name, track_number, nb_of_tracks)
+    printf('\n')
     
     
     
